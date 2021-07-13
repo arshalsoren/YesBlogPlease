0760d465bfd1a92186ba10b297ea762c348105ef
arshalsoren <arshalsoren@gmail.com>
2021-07-13 21:51:06 +0530

    update - fetching from database created with mongoose and displaying

--- app.js
+++ app.js
@@ -1,51 +1,80 @@
 const express = require('express');
 const { networkInterfaces } = require('os');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/backend-node');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', () => {
+    console.log('connected to mongoDB');
+})
+
+// Check for db errors
+db.on('error', (err) => {
+    console.log(err);
+});
 
 // Init App
 const app = express();
 
+// Bring in models
+let Page = require('./models/page');
+
 // Load View Engine
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (request, response) => {
-    let newPage=[
-        {
-            id:1,
-            title:"Blog #1",
-            author:"John Doe",
-            body:"Welcome to my blog one"
-        },
-        {
-            id:2,
-            title:"Blog #2",
-            author:"Edgar Allan Poe",
-            body:"Welcome to my blog two"
-        },
-        {
-            id:3,
-            title:"Blog #3",
-            author:"Elton JOhn",
-            body:"Welcome to my blog three"
+    Page.find({}, (err, pages) => {
+        if (err) {
+            console.log(err);
         }
-    ]
-    response.render('index', {
-        title:"Blogs",
-        pages:newPage
+        else {
+            response.render('index', {
+                title: 'Pages',
+                pages: pages
+            });
+        }
     });
+
+    // let newPage = [
+    //     {
+    //         id: 1,
+    //         title: "Blog #1",
+    //         author: "John Doe",
+    //         body: "Welcome to my blog one"
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "Blog #2",
+    //         author: "Edgar Allan Poe",
+    //         body: "Welcome to my blog two"
+    //     },
+    //     {
+    //         id: 3,
+    //         title: "Blog #3",
+    //         author: "Elton JOhn",
+    //         body: "Welcome to my blog three"
+    //     }
+    // ]
+    // response.render('index', {
+    //     title: "Blogs",
+    //     pages: newPage
+    // });
 });
 
 // Add New Page
-app.get('/pages/new',(request, response)=>{
+app.get('/pages/new', (request, response) => {
     response.render('new_page1', {
-        title:"New Page #1"
+        title: "New Page #1"
     });
 })
 
 
 // Start Server
-app.listen(3000, ()=>{
-console.log("Server started on 3000")
+app.listen(3000, () => {
+    console.log("Server started on 3000")
 });
