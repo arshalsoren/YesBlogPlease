c5aec3634da909c25a0907b1b785050eb1bb6a76
arshalsoren <arshalsoren@gmail.com>
2021-07-19 21:54:55 +0530

    fix: error message; express-validator middleware unwanted(removed)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { body, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
 
@@ -53,27 +54,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Express Validator Middleware
-app.post(
-    '/user',
-    // username must be an email
-    body('username').isEmail(),
-    // password must be at least 5 chars long
-    body('password').isLength({ min: 5 }),
-    (req, res) => {
-        // Finds the validation errors in this request and wraps them in an object with handy functions
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        User.create({
-            username: req.body.username,
-            password: req.body.password,
-        }).then(user => res.json(user));
-    },
-);
-
 // Home Route
 app.get('/', (request, response) => {
     Page.find({}, (err, pages) => {
@@ -107,39 +87,37 @@ app.get('/pages/add', (request, response) => {
 })
 
 // Add Submit POST Route
-app.post('/pages/add', (request, response) => {
-    // request.checkBody('title', 'Title is required').notEmpty();
-    // request.checkBody('author', 'Author is required').notEmpty();
-    // request.checkBody('body', 'Body is required').notEmpty();
-    
-    // for some reason checkBody() is not a function and hence an error is generated. 
-    // If you know how to resolve the issue, feel free to create and issue
+app.post('/pages/add',
+    check('title').not().isEmpty().withMessage('Title is required'),
+    check('author').not().isEmpty().withMessage('Author is required'),
+    check('body').not().isEmpty().withMessage('Body is required'),
 
     // Get Error
-    let errors = validationResult(request);
-    if (errors) {
-        response.render('add_page', {
-            title: "Add Page",
-            errors: errors
-        });
-    }
-    else {
-        let page = new Page();
-        page.title = request.body.title;
-        page.author = request.body.author;
-        page.body = request.body.body;
+    (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            response.render('add_page', {
+                title: "Add Page",
+                errors: errors
+            });
+        }
+        else {
+            let page = new Page();
+            page.title = request.body.title;
+            page.author = request.body.author;
+            page.body = request.body.body;
 
-        page.save((err) => {
-            if (err) {
-                console.log(err); return;
-            }
-            else {
-                request.flash('success', 'Page Added');
-                response.redirect('/');
-            }
-        });
-    }
-});
+            page.save((err) => {
+                if (err) {
+                    console.log(err); return;
+                }
+                else {
+                    request.flash('success', 'Page Added');
+                    response.redirect('/');
+                }
+            });
+        }
+    });
 
 // Load Edit Form
 app.get("/page/edit/:id", (request, response) => {
@@ -179,7 +157,7 @@ app.delete('/page/:id', (request, response) => {
         if (err) {
             console.log(err);
         }
-        request.flash('danger', 'Page Deleted');
+        request.flash('success', 'Page Deleted');
         response.send('Success');
     });
 });
